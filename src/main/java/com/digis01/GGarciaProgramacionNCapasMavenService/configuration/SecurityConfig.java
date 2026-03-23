fea92f3e9605e0b58ace84d2b14d9d98e1992d91
GabrielGarciaToriz <gabrielgtoriz@hotmail.com>
2026-03-23 16:07:16 -0600

    Implementation de JWT Y Security v 1.0.0

--- src/main/java/com/digis01/GGarciaProgramacionNCapasMavenService/configuration/SecurityConfig.java
+++ src/main/java/com/digis01/GGarciaProgramacionNCapasMavenService/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.digis01.GGarciaProgramacionNCapasMavenService.configuration;
 
 import com.digis01.GGarciaProgramacionNCapasMavenService.security.JwtAuthenticationFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -33,13 +34,22 @@ public class SecurityConfig {
                         "/swagger-ui/**",
                         "/swagger-ui.html"
                 ).permitAll()
+                
+                .requestMatchers("/api/usuario/**").hasAuthority("Administrador")
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             )
             .authenticationProvider(authenticationProvider)
-            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(exception -> exception
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN); // Código 403
+                    response.setContentType("application/json;charset=UTF-8"); // Para que acepte acentos
+                    response.getWriter().write("{\"status\": 403, \"message\": \"No tienes los permisos necesarios para ejecutar este endpoint.\"}");
+                })
+            );
 
         return http.build();
     }
