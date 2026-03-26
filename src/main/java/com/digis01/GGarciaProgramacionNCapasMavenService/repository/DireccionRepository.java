9f84abca33882d2688e94782a2daf35c5c576948
GabTrax <g.garcia.t839@edu.utc.mx>
2026-03-26 12:15:35 -0600

    Implementacion de roles

--- src/main/java/com/digis01/GGarciaProgramacionNCapasMavenService/repository/DireccionRepository.java
+++ src/main/java/com/digis01/GGarciaProgramacionNCapasMavenService/repository/DireccionRepository.java
@@ -4,8 +4,10 @@ import com.digis01.GGarciaProgramacionNCapasMavenService.entity.Direccion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface DireccionRepository extends JpaRepository<Direccion, Integer> {
     List<Direccion> findByUsuarioIdUsuario(int idUsuario);
+    Optional<Direccion> findByIdDireccionAndUsuarioIdUsuario(int idDireccion, int idUsuario);
 }
